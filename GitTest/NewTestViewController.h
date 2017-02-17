b341b00672edc8888adffc07140dc38b3a82335d
Andy Wu <andywu@Andys-Mac.local>
2017-02-17 13:36:29 -0500

    Added a new test file

---
+++ GitTest/NewTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewTestViewController.h
+//  GitTest
+//
+//  Created by Andy Wu on 2/17/17.
+//  Copyright © 2017 Avery Barrantes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewTestViewController : UIViewController
+
+@end
---
+++ GitTest/NewTestViewController.m
@@ -0,0 +1,39 @@
+//
+//  NewTestViewController.m
+//  GitTest
+//
+//  Created by Andy Wu on 2/17/17.
+//  Copyright © 2017 Avery Barrantes. All rights reserved.
+//
+
+#import "NewTestViewController.h"
+
+@interface NewTestViewController ()
+
+@end
+
+@implementation NewTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    NSLog(@"This is a new Test File");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
